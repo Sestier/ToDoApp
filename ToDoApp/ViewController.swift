9eab54e17fe09bd9467ff1fb54dfc52abedef5f3
René Sestier <rene_sestier@hotmail.com>
2020-11-22 10:38:46 -0700

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by mawetecnologías on 22/11/20.
+//  Copyright © 2020 sestier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
